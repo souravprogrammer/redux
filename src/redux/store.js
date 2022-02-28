7dc1922c48e6769bdd1045f55c5c837862248dfd
sourav <soruavsh1234567@gmail.com>
2022-02-28 17:31:25 +0530

    added logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,15 @@
-import redux, { createStore } from "redux"
-import { counterReducer , rootReducer , nameReducer} from "./counter/CounterReducer"
+import redux, { createStore , applyMiddleware } from "redux"
+import logger  from "redux-logger"
+import { counterReducer , rootReducer , nameReducer } from "./counter/CounterReducer"
 
 
-export const store = createStore(rootReducer)
+
+
+/**
+ * ${ways to use a create sore function }
+ *createStore(Reduser)
+ *createStore(Reduser, middleware)
+ *createStore(Reduser,initialState  , middleware)
+ *createStore(Reduser, initialState)
+ */
+export const store = createStore(rootReducer ,applyMiddleware(logger))
